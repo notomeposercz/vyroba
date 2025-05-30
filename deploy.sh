03c48f3b0d66fda637876c01f61d42c8e4667bf8
Mirek Urbánek <nothing@Mirek-MacBook-Air-2774.local>
2025-05-30 13:44:24 +0200

    Automatický commit: změny v projektu

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-# Automatický commit, push a nasazení na FTP pomocí standardního sftp
+# Automatický commit, push a nasazení na FTP pomocí SFTP na portu 222
 
 git add .
 git commit -m "Automatický commit: změny v projektu"
@@ -13,12 +13,12 @@ cd /_sub/vyroba/
 put -r .
 EOF
 
-# Spuštění sftp s heslem (nutno mít nainstalováno sshpass)
+# Spuštění sftp na portu 222 s heslem (nutno mít nainstalováno sshpass)
 if command -v sshpass >/dev/null 2>&1; then
-  sshpass -p '58xoSDKh' sftp -oBatchMode=no -oStrictHostKeyChecking=no myrec.cz@62.109.154.144 < $TMP_BATCH
+  sshpass -p '58xoSDKh' sftp -P 222 -oBatchMode=no -oStrictHostKeyChecking=no myrec.cz@62.109.154.144 < $TMP_BATCH
 else
   echo "Pro automatické zadání hesla nainstalujte sshpass: brew install hudochenkov/sshpass/sshpass"
-  echo "Nebo spusťte ručně: sftp myrec.cz@62.109.154.144 a použijte příkazy v $TMP_BATCH"
+  echo "Nebo spusťte ručně: sftp -P 222 myrec.cz@62.109.154.144 a použijte příkazy v $TMP_BATCH"
 fi
 
 rm $TMP_BATCH
